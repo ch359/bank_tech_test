0ec5b42e8ec69793dfd0615785e7050fdd96a935
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-03-11 14:29:02 +0000

    Refactor to DRY deposit and withdrawal methods

--- lib/bank.rb
+++ lib/bank.rb
@@ -10,24 +10,26 @@ class Bank
   attr_reader :balance
 
   def deposit(amount, date)
-    raise 'Invalid amount' if amount.nil? || amount <= 0
+    raise 'Invalid amount' if amount <= 0
+
+    record_transaction(amount, date)
+  end
+
+  def withdrawal(amount, date)
+    raise 'Invalid amount' if amount <= 0
+
+    record_transaction(-amount, date)
+  end
+
+  private
+
+  def record_transaction(amount, date)
     raise 'Invalid date' if date.nil?
+    raise 'Invalid amount' if amount.nil?
 
     new_balance = @balance += amount
     @transactions.push(
       amount: amount,
-      date: date,
-      balance: new_balance
-    )
-  end
-
-  def withdrawal(amount, date)
-    raise 'Invalid amount' if amount.nil? || amount <= 0
-    raise 'Invalid date' if date.nil?
-
-    new_balance = @balance -= amount
-    @transactions.push(
-      amount: -amount,
       date: date,
       balance: new_balance
     )
